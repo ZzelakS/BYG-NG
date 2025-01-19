fed3fa10a36a416573d4777e5b57efbf9ac619d8
ZzelakS <adamsskalezz@gmail.com>
2025-01-19 15:45:05 +0100

    changed image fit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -43,7 +43,7 @@ Let the good times roll – your stage awaits!
           loop
           playsInline
           muted
-          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
+          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4 object-cover"
         >
           <source src={video2} type="video/mp4" />
           Your browser does not support the video tag.
